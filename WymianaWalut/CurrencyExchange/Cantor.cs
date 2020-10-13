3837fa1727f0bb00ab01fc87ad21c68ddd21e33f
qumqum22 <64191277+qumqum22@users.noreply.github.com>
2020-10-13 16:37:06 +0200

    Add files via upload

---
+++ WymianaWalut/CurrencyExchange/Cantor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange
+{
+    class Cantor
+    {
+        public Currency[] Currencies { set; get; }
+
+
+    }
+}
+
+// Osobna klasa ktora laczy z xml i tworzy kolekcje walut
+
+// const string https://www.nbp.pl/kursy/xml/lasta.xml
---
+++ WymianaWalut/CurrencyExchange/Currency.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange
+{
+    class Currency
+    {
+        public string Name { get;protected set; }
+        public double Factor { get;protected set; }
+        public string CurrencyTag { get; protected set; }
+        public double ExchangeRate { get; protected set; }
+
+        public Currency()
+        {
+
+        }
+        public Currency(string name, double factor, string currencyTag, double exchangeRate)
+        {
+            this.Name = name;
+            this.Factor = factor;
+            this.CurrencyTag = currencyTag;
+            this.ExchangeRate = exchangeRate;
+        }
+
+    }
+}
