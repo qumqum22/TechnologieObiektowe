1c6af9240e04a2dd56f9806601850846fd7e8d50
qumqum22 <64191277+qumqum22@users.noreply.github.com>
2021-04-03 14:47:13 +0200

    Add files via upload
    Simple application for exercise drag and drop operations in forms.

---
+++ DragAndDrop/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragAndDrop
+{
+    public partial class Form1 : Form
+    {
+        object dragDropSource = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 10; ++i)
+            {
+                listBox1.Items.Add("lista 1, pozycja " + i.ToString());
+                listBox2.Items.Add("lista 2, pozycja " + i.ToString());
+            }
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListBox lbSender = sender as ListBox;
+            dragDropSource = sender; //przechowanie referencji dla DragOver
+            
+            if(e.Button == MouseButtons.Left && lbSender.SelectedIndices.Count > 0)
+            {
+                DragDropEffects operacja = lbSender.DoDragDrop(lbSender.SelectedItems, DragDropEffects.Copy | DragDropEffects.Move);
+
+                if(operacja == DragDropEffects.Move)
+                {
+                    for (int i = lbSender.SelectedItems.Count - 1; i >= 0; i--)
+                        lbSender.Items.Remove(lbSender.SelectedItems[i]);
+                }
+            }
+            dragDropSource = null;
+        }
+
+        private void listBox2_DragOver(object sender, DragEventArgs e)
+        {
+            if(sender == dragDropSource)
+                e.Effect = DragDropEffects.None;
+                else
+                if ((e.KeyState & 8) == 8)
+                    e.Effect = DragDropEffects.Copy; // Ctrl + Z
+                    else
+                    e.Effect = DragDropEffects.Move;
+            
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            ListBox lbSender = sender as ListBox;
+            ListBox lbSource = dragDropSource as ListBox;
+            int indeks = lbSender.IndexFromPoint(lbSender.PointToClient(new Point(e.X, e.Y)));
+            if (indeks == -1) indeks = lbSender.Items.Count;
+            for (int i = lbSource.SelectedItems.Count - 1; i >= 0; i--)
+                lbSender.Items.Insert(indeks, lbSource.Items[lbSource.SelectedIndices[i]]);
+        }
+
+
+    }
+}
